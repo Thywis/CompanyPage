124a15e7b6a003c1fdbcfc3eb95eba64516d93ba
Liby99 <liby99@icloud.com>
2017-05-03 20:42:47 -0700

    Fixed Navbar Js

--- js/landing.js
+++ js/landing.js
@@ -1,8 +1,25 @@
 $(function () {
     
+    var $sectionCache = $("section");
+    
+    function refreshNavbar() {
+        var scrollTop = $(window).scrollTop();
+        var a = 0;
+        for (var i = 0; i < $sectionCache.length; i++) {
+            var $section = $sectionCache.eq(i);
+            var top = $section.offset().top;
+            var height = $section.outerHeight();
+            if (top < scrollTop + 3 && top + height > scrollTop + 3) {
+                a = i;
+            }
+        }
+        $("#page-nav a").removeClass("active");
+        $("#page-nav a[data-href='#" + $sectionCache.eq(a).attr("id") + "']").addClass("active");
+    }
+    
     //
     $(window).scroll(function () {
-        
+        refreshNavbar();
     });
     
     $("#page-nav a").click(function () {
@@ -12,4 +29,6 @@ $(function () {
             scrollTop: $(sec).offset().top
         }, 500);
     });
+    
+    refreshNavbar();
 });
